602bd1f3cdea3ca4e995e1611ccb291521d9b3fb
tuananh <tuananh2431998@gmail.com>
2022-01-17 23:12:06 +0700

    singleton db

--- news-gui/src/database/DbConnection.java
+++ news-gui/src/database/DbConnection.java
@@ -4,17 +4,19 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 public class DbConnection {
-	  public static Connection connect() {
-		    Connection con = null; 
-		    try {
-		      Class.forName("org.sqlite.JDBC");
-		      con = DriverManager.getConnection("jdbc:sqlite:news.db"); // connecting to our database
-		      System.out.println("Connected!");
-		    } catch (ClassNotFoundException | SQLException e ) {
-		      // TODO Auto-generated catch block
-		      System.out.println(e+"");
-		    }
-		    
-		    return con; 
-		  }
+	public static Connection connection;
+	static {
+		try {
+			Class.forName("org.sqlite.JDBC");
+			connection = DriverManager.getConnection("jdbc:sqlite:news.db"); // connecting to our database
+			System.out.println("Connected!");
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println(e + "");
+		}
+	}
+
+	public static Connection connect() {
+		return connection;
+	}
 }
